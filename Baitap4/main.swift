efc5c6b3cdb8609234d8f022ea05ee51e08a1997
Trương Minh Lượng Jr <truongminhluongjr@Truong-Minh-Luong-2.local>
2023-10-08 09:18:31 +0700

    init repository

---
+++ Baitap4/main.swift
@@ -0,0 +1,41 @@
+//
+//  main.swift
+//  Baitap4
+//
+//  Created by Trương Minh Lượng Jr on 08/10/2023.
+//
+
+import Foundation
+
+func findFirstOddAndLastEvenPosition(_ numbers: [Int]) -> (Int?, Int?) {
+    var firstOddPosition: Int? = nil
+    var lastEvenPosition: Int? = nil
+    
+    for (index, number) in numbers.enumerated() {
+        if number % 2 == 1 && firstOddPosition == nil {
+            firstOddPosition = index
+        }
+        
+        if number % 2 == 0 {
+            lastEvenPosition = index
+        }
+    }
+    
+    return (firstOddPosition, lastEvenPosition)
+}
+
+let numbers = [2, 4, 6, 1, 8, 10, 3]
+let positions = findFirstOddAndLastEvenPosition(numbers)
+
+if let firstOddPosition = positions.0 {
+    print("Vị trí đầu tiên của số lẻ: \(firstOddPosition)")
+} else {
+    print("Không có số lẻ trong mảng.")
+}
+
+if let lastEvenPosition = positions.1 {
+    print("Vị trí cuối cùng của số chẵn: \(lastEvenPosition)")
+} else {
+    print("Không có số chẵn trong mảng.")
+}
+
